596e943f67c31c27fc3964538a815326e1712419
Eazybel <eazybel27@gmail.com>
2023-11-19 00:25:09 +0300

    number_guss_game_added

---
+++ number_guess_game.py
@@ -0,0 +1,15 @@
+import random
+
+sec=random.randint(1,10)
+choice=int(input("\t\t Enter your guess 1-10\n..."))
+if choice == sec:
+    print("congrats you've won 100000$")
+elif choice != sec:
+    print("try again next time")
+
+       
+    
+    
+    
+    
+    
